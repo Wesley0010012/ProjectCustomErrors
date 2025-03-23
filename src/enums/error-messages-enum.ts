0ac02ce3177d111edd5381c822d2fdd6a4ed6f95
Wesley0010012 <wglaurindo33@gmail.com>
2025-03-23 07:16:42 -0300

    feat: add default error messages enum

---
+++ src/enums/error-messages-enum.ts
@@ -0,0 +1,12 @@
+export enum ErrorMessagesEnum {
+  INTERNAL_ERROR = 'Internal Error',
+  INVALID_PARAM_ERROR = 'Invalid Param',
+  MISSING_PARAM_ERROR = 'Missing Param',
+  NOT_FOUND = 'Not Found',
+  VALIDATION_ERROR = 'Validation Error',
+  DOMAIN_ERROR = 'Domain Error',
+  UNAUTHENTICATED = 'Unauthenticated',
+  UNAUTHORIZED = 'Unauthorized',
+  AUTHENTICATION_ERROR = 'Authentication Error',
+  ENTITY_ERROR = 'Entity Error'
+}
